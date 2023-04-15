d852a71f6b8d2fc917cb796a6c99051c1b676103
ghitar1 <122742826+ghitar1@users.noreply.github.com>
2023-04-15 17:06:22 -0500

    Create war_deploy_sample.java

---
+++ war_deploy_sample.java
@@ -0,0 +1,10 @@
+/**
+ * based on content from Crunchify.com
+ *
+ */
+public class wardeploy {
+    public static void main(String[] args) {
+        System.out
+                .println("Test.. Test by Derek. \nThis is a simple tutorial on how to create .war file using Maven Plugin..");
+    }
+}
